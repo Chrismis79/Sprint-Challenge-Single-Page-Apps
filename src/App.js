3daa65e4ead1c8c821efcaa175fd5041b118e780
Chrismis79 <chrismis79@gmail.com>
2019-09-20 12:00:05 -0500

    routes and links set up and working, home page imported

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header.js";
+import {Router, Route, Link} from "react-router-dom";
 
+import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 
 
 export default function App() {
+  const [people] = useState()
   return (
     <main>
       <Header />
-      <CharacterList/>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/character-list">Characters</Link>
+        </nav>
+
+        <Route exact path="/" component={WelcomePage}/>
+        <Route exact path="/character-list" render={props => (
+          <CharacterList
+          {...props}
+          items={people} />
+
+        )} />
+      </div>
+      
     </main>
   );
 }
