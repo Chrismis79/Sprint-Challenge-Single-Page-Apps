892b53ec99fbf5f5ea5969b74f4bb6ed23090715
Chrismis79 <chrismis79@gmail.com>
2019-09-20 11:12:17 -0500

    axios call working and data displaying

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+  <div>
+    <p>{props.name}</p>
+    <p>{props.name} is curently {props.status} and is of the {props.species} species.</p>
+  </div>
+   );
 }
