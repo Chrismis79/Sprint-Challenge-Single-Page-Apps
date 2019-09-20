e405ff414de2de4611b5a7fe37bcd0d1e973544d
Chrismis79 <chrismis79@gmail.com>
2019-09-20 11:35:41 -0500

    character list styled with styled-components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const CharaCardDiv = styled.div`
+  border: 1px solid black;
+  background: grey;
+  width: 50%;
+  color: white;
+  margin: 1% 0
+`;
+const CharacterCardContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+`;
 
 export default function CharacterCard(props) {
   return (
-  <div>
-    <p>{props.name}</p>
-    <p>{props.name} is curently {props.status} and is of the {props.species} species.</p>
-  </div>
+    <CharacterCardContainer> 
+      <CharaCardDiv>
+        <p>{props.name}</p>
+        <p>{props.name} is curently {props.status} and is of the {props.species} species.</p>
+      </CharaCardDiv>
+    </CharacterCardContainer>
    );
 }
